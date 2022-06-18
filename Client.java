9fb37f0b8006845cdf5d96e36fadb9381cfb07a3
PedroA27 <94874934+PedroA27@users.noreply.github.com>
2022-06-18 04:05:42 -0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,4 +1,55 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Scanner;
+import java.util.UUID;
+import java.util.Collection;
 
 public class Client {
+	PartRepository repository;
+	private Part part;
+	
+	public Client(String server) throws MalformedURLException, RemoteException, NotBoundException {
+		this.repository = (PartRepository) Naming.lookup(server);
+		this.part = null;
+	}
+	
+	
+	public void bind(String server) throws MalformedURLException, RemoteException, NotBoundException {
+		this.repository = (PartRepository) Naming.lookup(server);
+	
+	}
+	
+	public Collection<Part> listp() {
+		try {
+			Collection<Part> list = repository.listParts();
+			return list;
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	public Part getp(UUID partId) {
+		try {
+			return repository.getPart(partId);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	 public void setPart(Part newPart) {
+
+	        this.part = newPart;
+    }
+	 public Part getPart() {
+			return this.part;
+		}
 
 }
